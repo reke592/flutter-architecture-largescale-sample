8c6fb0b8847fd590019e1f80ac1cf9c12c1df742
Erric Rapsing <erric.rapsing@gmail.com>
2024-01-30 20:40:49 +0800

    CHORE - remove comments in default test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:largescale/main.dart';
@@ -12,12 +5,10 @@ import 'package:largescale/src/core/bootstrap.dart';
 
 void main() {
   testWidgets('main bootstrap', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await bootstrap();
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle(const Duration(seconds: 1));
 
-    // Verify that our counter starts at 0.
     expect(find.text('Sign-in'), findsOneWidget);
     expect(find.text('Dashboard'), findsNothing);
   });
